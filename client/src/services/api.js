eceb0071f64b860ba6ef9e7065d159463cf9365c
PrashunaPulami <prashunamgr07@gmail.com>
2026-08-12 11:34:25 +0545

    login fix 2

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://inventory-management-system-8eue.onrender.com/api"
+    baseURL: "https://inventory-management-system-8eue.onrender.com/api/"
 });
 
 api.interceptors.request.use((config) => {
